50a16e0ecadc06d474e22a6ba910b7cbbc144320
brightowltutoring <106844324+brightowltutoring@users.noreply.github.com>
2023-01-01 00:15:48 -0500

    third committ??

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,8 @@
 import { mdsvex } from "mdsvex";
 import mdsvexConfig from "./mdsvex.config.js";
 import preprocess from "svelte-preprocess";
-import adapter from "@sveltejs/adapter-auto";
-// import adapter from "@sveltejs/adapter-vercel";
+// import adapter from "@sveltejs/adapter-auto";
+import adapter from "@sveltejs/adapter-vercel";
 // import adapter from "@sveltejs/adapter-cloudflare";
 
 /** @type {import('@sveltejs/kit').Config} */
